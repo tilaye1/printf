453fbb1dac00a4cde91de0f7c779fc82b23a18fc
tilaye1 <til_mer@yahoo.com>
2022-07-12 11:51:09 +0300

    commented

--- _printf.c
+++ _printf.c
@@ -1,103 +1,52 @@
 #include "main.h"
 
 /**
- * check_buffer_overflow - if writing over buffer space,
- * print everything then revert length back to 0 to write at buffer start
- * @buffer: buffer holding string to print
- * @len: position in buffer
- * Return: length position
+ * _printf - prints
+ * @format: the first parameter
+ *
+ * Return: number of printed char
  */
-int check_buffer_overflow(char *buffer, int len)
-{
-	if (len > 1020)
-	{
-		write(1, buffer, len);
-		len = 0;
-	}
-	return (len);
-}
 
-
-/**
- * _printf - mini printf version
- * @format: initial string with all identifiers
- * Return: strings with identifiers expanded
- */
 int _printf(const char *format, ...)
 {
-	int len = 0, total_len = 0, i = 0, j = 0;
-	va_list list;
-	char *buffer, *str;
-	char* (*f)(va_list);
+	char *ptr, *start;
 
-	if (format == NULL)
+	flags_t fgs = FLAGS_INIT;
+	int cp = 0;
+	int (*pF)(va_list, flags_t *);
+	va_list ap;
+
+	va_start(ap, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	buffer = create_buffer();
-	if (buffer == NULL)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-
-	va_start(list, format);
-
-	while (format[i] != '\0')
+	for (ptr = (char *)format; *ptr; ptr++)
 	{
-		if (format[i] != '%') /* copy format into buffer until '%' */
+		if (*ptr == '%')
 		{
-			len = check_buffer_overflow(buffer, len);
-			buffer[len++] = format[i++];
-			total_len++;
-		}
-		else /* if %, find function */
-		{
-			i++;
-			if (format[i] == '\0') /* handle single ending % */
+			ptr++;
+			if (*ptr == '%')
 			{
-				va_end(list);
-				free(buffer);
-				return (-1);
+				cp += _putchar(*ptr);
+				continue;
 			}
-			if (format[i] == '%') /* handle double %'s */
+			start = ptr;
+			while (getFlags(*ptr, &fgs))
+				ptr++;
+			pF = getPrint(*ptr);
+			if (!pF)
 			{
-				len = check_buffer_overflow(buffer, len);
-				buffer[len++] = format[i];
-				total_len++;
+				cp += _putchar('%');
+				ptr = start - 1;
 			}
 			else
-			{
-				f = get_func(format[i]); /* grab function */
-				if (f == NULL)  /* handle fake id */
-				{
-					len = check_buffer_overflow(buffer, len);
-					buffer[len++] = '%'; total_len++;
-					buffer[len++] = format[i]; total_len++;
-				}
-				else /* return string, copy to buffer */
-				{
-					str = f(list);
-					if (str == NULL)
-					{
-						va_end(list);
-						free(buffer);
-						return (-1);
-					}
-					if (format[i] == 'c' && str[0] == '\0')
-					{
-						len = check_buffer_overflow(buffer, len);
-						buffer[len++] = '\0';
-						total_len++;
-					}
-					j = 0;
-					while (str[j] != '\0')
-					{
-						len = check_buffer_overflow(buffer, len);
-						buffer[len++] = str[j];
-						total_len++; j++;
-					}
-					free(str);
-				}
-			} i++;
+				cp += pF(ap, &fgs);
 		}
+		else
+			cp += _putchar(*ptr);
 	}
-	write_buffer(buffer, len, list);
-	return (total_len);
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (cp);
 }
